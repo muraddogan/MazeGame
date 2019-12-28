b6888d4a10e92d9815c3dd35731de153b7685b55
Murat Dogan <37879444+muraddogan@users.noreply.github.com>
2019-12-28 22:08:14 +0300

    MazeGame

---
+++ LabirentProblemi/LabirentProblemi/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace LabirentProblemi
+{
+    partial class FormLabirent
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLabirent));
+            this.buttonBilgisayar = new System.Windows.Forms.Button();
+            this.buttonTekOyuncu = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelYol = new System.Windows.Forms.Label();
+            this.pictureBoxYol = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDuvar = new System.Windows.Forms.PictureBox();
+            this.labelDuvar = new System.Windows.Forms.Label();
+            this.buttonUst = new System.Windows.Forms.Button();
+            this.buttonSol = new System.Windows.Forms.Button();
+            this.buttonSag = new System.Windows.Forms.Button();
+            this.buttonAlt = new System.Windows.Forms.Button();
+            this.timerOyun = new System.Windows.Forms.Timer(this.components);
+            this.labelHamle = new System.Windows.Forms.Label();
+            this.labelZaman = new System.Windows.Forms.Label();
+            this.progressBarZaman = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDuvar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBilgisayar
+            // 
+            this.buttonBilgisayar.Location = new System.Drawing.Point(0, 0);
+            this.buttonBilgisayar.Name = "buttonBilgisayar";
+            this.buttonBilgisayar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBilgisayar.TabIndex = 0;
+            this.buttonBilgisayar.Text = "Bilgisayar";
+            this.buttonBilgisayar.UseVisualStyleBackColor = true;
+            this.buttonBilgisayar.Click += new System.EventHandler(this.buttonBilgisayar_Click);
+            // 
+            // buttonTekOyuncu
+            // 
+            this.buttonTekOyuncu.Location = new System.Drawing.Point(81, 0);
+            this.buttonTekOyuncu.Name = "buttonTekOyuncu";
+            this.buttonTekOyuncu.Size = new System.Drawing.Size(75, 23);
+            this.buttonTekOyuncu.TabIndex = 1;
+            this.buttonTekOyuncu.Text = "Tek Oyuncu";
+            this.buttonTekOyuncu.UseVisualStyleBackColor = true;
+            this.buttonTekOyuncu.Click += new System.EventHandler(this.buttonTekOyuncu_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(398, 0);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // labelYol
+            // 
+            this.labelYol.AutoSize = true;
+            this.labelYol.Location = new System.Drawing.Point(194, 9);
+            this.labelYol.Name = "labelYol";
+            this.labelYol.Size = new System.Drawing.Size(22, 13);
+            this.labelYol.TabIndex = 3;
+            this.labelYol.Text = "Yol";
+            // 
+            // pictureBoxYol
+            // 
+            this.pictureBoxYol.BackColor = System.Drawing.Color.White;
+            this.pictureBoxYol.Location = new System.Drawing.Point(162, 0);
+            this.pictureBoxYol.Name = "pictureBoxYol";
+            this.pictureBoxYol.Size = new System.Drawing.Size(26, 23);
+            this.pictureBoxYol.TabIndex = 4;
+            this.pictureBoxYol.TabStop = false;
+            this.pictureBoxYol.Click += new System.EventHandler(this.pictureBoxYol_Click);
+            // 
+            // pictureBoxDuvar
+            // 
+            this.pictureBoxDuvar.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxDuvar.Location = new System.Drawing.Point(235, 0);
+            this.pictureBoxDuvar.Name = "pictureBoxDuvar";
+            this.pictureBoxDuvar.Size = new System.Drawing.Size(26, 23);
+            this.pictureBoxDuvar.TabIndex = 5;
+            this.pictureBoxDuvar.TabStop = false;
+            this.pictureBoxDuvar.Click += new System.EventHandler(this.pictureBoxDuvar_Click);
+            // 
+            // labelDuvar
+            // 
+            this.labelDuvar.AutoSize = true;
+            this.labelDuvar.Location = new System.Drawing.Point(267, 9);
+            this.labelDuvar.Name = "labelDuvar";
+            this.labelDuvar.Size = new System.Drawing.Size(36, 13);
+            this.labelDuvar.TabIndex = 6;
+            this.labelDuvar.Text = "Duvar";
+            // 
+            // buttonUst
+            // 
+            this.buttonUst.Image = ((System.Drawing.Image)(resources.GetObject("buttonUst.Image")));
+            this.buttonUst.Location = new System.Drawing.Point(398, 64);
+            this.buttonUst.Name = "buttonUst";
+            this.buttonUst.Size = new System.Drawing.Size(75, 60);
+            this.buttonUst.TabIndex = 7;
+            this.buttonUst.UseVisualStyleBackColor = true;
+            this.buttonUst.Click += new System.EventHandler(this.buttonUst_Click);
+            // 
+            // buttonSol
+            // 
+            this.buttonSol.Image = ((System.Drawing.Image)(resources.GetObject("buttonSol.Image")));
+            this.buttonSol.Location = new System.Drawing.Point(358, 130);
+            this.buttonSol.Name = "buttonSol";
+            this.buttonSol.Size = new System.Drawing.Size(75, 60);
+            this.buttonSol.TabIndex = 8;
+            this.buttonSol.UseVisualStyleBackColor = true;
+            this.buttonSol.Click += new System.EventHandler(this.buttonSol_Click);
+            // 
+            // buttonSag
+            // 
+            this.buttonSag.Image = ((System.Drawing.Image)(resources.GetObject("buttonSag.Image")));
+            this.buttonSag.Location = new System.Drawing.Point(439, 130);
+            this.buttonSag.Name = "buttonSag";
+            this.buttonSag.Size = new System.Drawing.Size(75, 60);
+            this.buttonSag.TabIndex = 9;
+            this.buttonSag.UseVisualStyleBackColor = true;
+            this.buttonSag.Click += new System.EventHandler(this.buttonSag_Click);
+            // 
+            // buttonAlt
+            // 
+            this.buttonAlt.Image = ((System.Drawing.Image)(resources.GetObject("buttonAlt.Image")));
+            this.buttonAlt.Location = new System.Drawing.Point(398, 196);
+            this.buttonAlt.Name = "buttonAlt";
+            this.buttonAlt.Size = new System.Drawing.Size(75, 60);
+            this.buttonAlt.TabIndex = 10;
+            this.buttonAlt.UseVisualStyleBackColor = true;
+            this.buttonAlt.Click += new System.EventHandler(this.buttonAlt_Click);
+            // 
+            // timerOyun
+            // 
+            this.timerOyun.Tick += new System.EventHandler(this.timerOyun_Tick);
+            // 
+            // labelHamle
+            // 
+            this.labelHamle.AutoSize = true;
+            this.labelHamle.Location = new System.Drawing.Point(411, 307);
+            this.labelHamle.Name = "labelHamle";
+            this.labelHamle.Size = new System.Drawing.Size(37, 13);
+            this.labelHamle.TabIndex = 11;
+            this.labelHamle.Text = "Hamle";
+            // 
+            // labelZaman
+            // 
+            this.labelZaman.AutoSize = true;
+            this.labelZaman.Location = new System.Drawing.Point(474, 415);
+            this.labelZaman.Name = "labelZaman";
+            this.labelZaman.Size = new System.Drawing.Size(40, 13);
+            this.labelZaman.TabIndex = 12;
+            this.labelZaman.Text = "Zaman";
+            // 
+            // progressBarZaman
+            // 
+            this.progressBarZaman.Location = new System.Drawing.Point(12, 415);
+            this.progressBarZaman.Name = "progressBarZaman";
+            this.progressBarZaman.Size = new System.Drawing.Size(439, 23);
+            this.progressBarZaman.TabIndex = 13;
+            // 
+            // FormLabirent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 450);
+            this.Controls.Add(this.progressBarZaman);
+            this.Controls.Add(this.labelZaman);
+            this.Controls.Add(this.labelHamle);
+            this.Controls.Add(this.buttonAlt);
+            this.Controls.Add(this.buttonSag);
+            this.Controls.Add(this.buttonSol);
+            this.Controls.Add(this.buttonUst);
+            this.Controls.Add(this.labelDuvar);
+            this.Controls.Add(this.pictureBoxDuvar);
+            this.Controls.Add(this.pictureBoxYol);
+            this.Controls.Add(this.labelYol);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonTekOyuncu);
+            this.Controls.Add(this.buttonBilgisayar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormLabirent";
+            this.Text = "Labirent Problemi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDuvar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBilgisayar;
+        private System.Windows.Forms.Button buttonTekOyuncu;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelYol;
+        private System.Windows.Forms.PictureBox pictureBoxYol;
+        private System.Windows.Forms.PictureBox pictureBoxDuvar;
+        private System.Windows.Forms.Label labelDuvar;
+        private System.Windows.Forms.Button buttonUst;
+        private System.Windows.Forms.Button buttonSol;
+        private System.Windows.Forms.Button buttonSag;
+        private System.Windows.Forms.Button buttonAlt;
+        private System.Windows.Forms.Timer timerOyun;
+        private System.Windows.Forms.Label labelHamle;
+        private System.Windows.Forms.Label labelZaman;
+        private System.Windows.Forms.ProgressBar progressBarZaman;
+    }
+}
+
---
+++ LabirentProblemi/LabirentProblemi/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabirentProblemi
+{
+    public partial class FormLabirent : Form
+    {
+        Color anlikRenk = Color.White;
+        private int xBoyut = 10;
+        private int yBoyut = 10;
+        private int boyutDeger = 30;
+        private int xP = 0, yP = 0;
+        public static int temp = 0, sag = 0, alt = 0, sayac = 100;
+
+        
+
+        private PictureBox[,] labirentBoyut;
+
+        public FormLabirent()
+        {
+            InitializeComponent();
+            yeniLabOlustur();
+            labelHamle.Text = ("Hamle: " + temp);
+            buttonUst.Visible = false;
+            buttonSol.Visible = false;
+            buttonSag.Visible = false;
+            buttonAlt.Visible = false;
+            labelZaman.Text = ("0");
+            timerOyun.Interval = 100;
+            progressBarZaman.Value = sayac;
+            labelZaman.Text = sayac.ToString();
+        }
+
+
+        private void yeniLabOlustur()
+        {
+
+            labirentBoyut = new PictureBox[xBoyut, yBoyut];
+
+            for (int i = 0; i < xBoyut; i++)
+            {
+                for (int j = 0; j < yBoyut; j++)
+                {
+                    labirentBoyut[i, j] = new PictureBox();
+
+                    int xPosition = (i * boyutDeger) + 13;
+                    int yPosition = (j * boyutDeger) + 45;
+                    labirentBoyut[i, j].SetBounds(xPosition, yPosition, boyutDeger, boyutDeger);
+                    
+                    if ((i == 0 && j == 0) || (i == xBoyut - 1 && j == yBoyut - 1))
+                        labirentBoyut[i, j].BackColor = Color.LightBlue;
+                    else
+                    {
+                        labirentBoyut[i, j].BackColor = Color.White;
+                        EventHandler clickEvent = new EventHandler(PictureBox_Click);
+                        labirentBoyut[i, j].Click += clickEvent;
+                    }
+
+                    this.Controls.Add(labirentBoyut[i, j]);
+                }
+            }
+        }
+
+        private void pictureBoxYol_Click(object sender, EventArgs e)
+        {
+            anlikRenk = Color.White;
+        }
+
+        private void pictureBoxDuvar_Click(object sender, EventArgs e)
+        {
+            anlikRenk = Color.Black;
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            ((PictureBox)sender).BackColor = anlikRenk;
+        }
+
+       
+
+        private void buttonBilgisayar_Click(object sender, EventArgs e)
+        {
+            buttonTekOyuncu.Visible = false;
+
+            bool[,] bakilmisYol = new bool[xBoyut, yBoyut];
+
+            if (!labirentCoz(0, 0, bakilmisYol))
+                MessageBox.Show("Labirent Çözülemez.");
+        }
+
+        private void buttonTekOyuncu_Click(object sender, EventArgs e)
+        {
+            buttonBilgisayar.Visible = false;
+            buttonSag.Visible = true;
+            buttonAlt.Visible = true;
+            labirentBoyut[0, 0].BackColor = Color.Gray;
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            xP = 0; yP = 0; sag = 0; alt = 0;
+
+            buttonBilgisayar.Visible = true;
+            buttonTekOyuncu.Visible = true;
+            buttonUst.Visible = false;
+            buttonSol.Visible = false;
+            buttonSag.Visible = false;
+            buttonAlt.Visible = false;
+
+            timerOyun.Stop();
+            sayac = 100;
+            progressBarZaman.Value = sayac;
+            labelZaman.Text = sayac.ToString();
+
+            temp = 0;
+            labelHamle.Text = ("Hamle: " + temp);
+
+            for (int i = 0; i < xBoyut; i++)
+            {
+                for (int j = 0; j < yBoyut; j++)
+                {
+                    if (labirentBoyut[i, j].BackColor == Color.Gray)
+                        labirentBoyut[i, j].BackColor = Color.White;
+                    if (labirentBoyut[i, j].BackColor == Color.Red)
+                        labirentBoyut[i, j].BackColor = Color.White;
+                }
+            }
+
+            labirentBoyut[0, 0].BackColor = Color.LightBlue;
+            labirentBoyut[xBoyut - 1, yBoyut - 1].BackColor = Color.LightBlue;
+        }
+
+        private bool labirentCoz(int xPos, int yPos, bool[,] bakilmisYol)
+        {
+            bool dogruYol = false;
+
+            bool yolDogruMu = true;
+
+            if (xPos >= xBoyut || xPos < 0 || yPos >= yBoyut || yPos < 0)
+                yolDogruMu = false;
+            else
+            {
+                if (labirentBoyut[xPos, yPos].BackColor == Color.LightBlue && (xPos != 0 && yPos != 0))
+                {
+                    dogruYol = true;
+                    yolDogruMu = false;
+                }
+
+                if (labirentBoyut[xPos, yPos].BackColor == Color.Black)
+                    yolDogruMu = false;
+
+                if (bakilmisYol[xPos, yPos])
+                    yolDogruMu = false;
+            }
+
+            if (yolDogruMu)
+            {
+
+                bakilmisYol[xPos, yPos] = true;
+
+                dogruYol = dogruYol || labirentCoz(xPos + 1, yPos, bakilmisYol);
+                dogruYol = dogruYol || labirentCoz(xPos, yPos + 1, bakilmisYol);
+                dogruYol = dogruYol || labirentCoz(xPos - 1, yPos, bakilmisYol);
+                dogruYol = dogruYol || labirentCoz(xPos, yPos - 1, bakilmisYol);
+
+                temp++;
+            }
+
+            if (dogruYol)
+                labirentBoyut[xPos, yPos].BackColor = Color.Gray;
+            labelHamle.Text = ("Hamle: " + temp);
+            return dogruYol;
+        }
+
+       
+
+        private void buttonUst_Click(object sender, EventArgs e)
+        {
+            --alt;
+            Control(sag, alt);
+            ++temp;
+            labelHamle.Text = ("Hamle: " + temp);
+            bool dogruYol = false;
+            bool yolDogruMu = true;
+
+            if (xP >= xBoyut || xP < 0 || yP >= yBoyut || yP < 0)
+                yolDogruMu = false;
+            else
+            {
+                if (labirentBoyut[xP, yP].BackColor == Color.LightBlue && (xP != 0 && yP != 0))
+                {
+                    dogruYol = true;
+                    yolDogruMu = false;
+                }
+
+                if (yP != 0)
+                {
+                    if (labirentBoyut[xP, yP - 1].BackColor == Color.Black)
+                        yolDogruMu = false;
+
+                }
+            }
+
+            if (yolDogruMu)
+            {
+
+                yP = yP - 1;
+                dogruYol = true;
+                yolDogruMu = false;
+
+            }
+
+            if (dogruYol)
+            {
+                if (yP == yBoyut - 1)
+                {
+
+                }
+                else
+                {
+                    labirentBoyut[xP, yP + 1].BackColor = Color.Gray;
+                    labirentBoyut[xP, yP].BackColor = Color.Red;
+
+                }
+            }
+        }
+
+        private void buttonSol_Click(object sender, EventArgs e)
+        {
+            --sag;
+            Control(sag, alt);
+            ++temp;
+            labelHamle.Text = ("Hamle: " + temp);
+            bool dogruYol = false;
+            bool yolDogruMu = true;
+
+            if (xP >= xBoyut || xP < 0 || yP >= yBoyut || yP < 0)
+                yolDogruMu = false;
+            else
+            {
+                if (labirentBoyut[xP, yP].BackColor == Color.LightBlue && (xP != 0 && yP != 0))
+                {
+                    dogruYol = true;
+                    yolDogruMu = false;
+                }
+
+
+                if (xP != 0)
+                {
+                    if (labirentBoyut[xP - 1, yP].BackColor == Color.Black)
+                        yolDogruMu = false;
+
+                }
+
+            }
+
+            if (yolDogruMu)
+            {
+
+                xP = xP - 1;
+                dogruYol = true;
+                yolDogruMu = false;
+
+            }
+
+            if (dogruYol)
+            {
+                if (xP == xBoyut - 1)
+                {
+
+                }
+                else
+                {
+                    labirentBoyut[xP + 1, yP].BackColor = Color.Gray;
+                    labirentBoyut[xP, yP].BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void buttonSag_Click(object sender, EventArgs e)
+        {
+            ++sag;
+            Control(sag, alt);
+            ++temp;
+            labelHamle.Text = ("Hamle: " + temp);
+            bool dogruYol = false;
+            bool yolDogruMu = true;
+
+            if (xP >= xBoyut || xP < 0 || yP >= yBoyut || yP < 0)
+                yolDogruMu = false;
+            else
+            {
+                if (labirentBoyut[xP, yP].BackColor == Color.LightBlue && (xP != 0 && yP != 0))
+                {
+                    dogruYol = true;
+                    yolDogruMu = false;
+                }
+
+
+                if (xP != xBoyut - 1)
+                {
+                    if (labirentBoyut[xP + 1, yP].BackColor == Color.Black)
+                        yolDogruMu = false;
+
+                }
+
+
+            }
+
+            if (yolDogruMu)
+            {
+
+                xP = xP + 1;
+                dogruYol = true;
+                yolDogruMu = false;
+
+            }
+
+            if (dogruYol)
+            {
+                if (xP == 0)
+                {
+
+                }
+                else
+                {
+                    labirentBoyut[xP - 1, yP].BackColor = Color.Gray;
+                    labirentBoyut[xP, yP].BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void buttonAlt_Click(object sender, EventArgs e)
+        {
+            ++alt;
+            Control(sag, alt);
+            ++temp;
+            labelHamle.Text = ("Hamle: " + temp);
+            bool dogruYol = false;
+            bool yolDogruMu = true;
+
+            if (xP >= xBoyut || xP < 0 || yP >= yBoyut || yP < 0)
+                yolDogruMu = false;
+            else
+            {
+                if (labirentBoyut[xP, yP].BackColor == Color.LightBlue && (xP != 0 && yP != 0))
+                {
+                    dogruYol = true;
+                    yolDogruMu = false;
+                }
+
+
+                if (yP != yBoyut - 1)
+                {
+                    if (labirentBoyut[xP, yP + 1].BackColor == Color.Black)
+                        yolDogruMu = false;
+
+                }
+
+
+            }
+
+            if (yolDogruMu)
+            {
+
+                yP = yP + 1;
+                dogruYol = true;
+                yolDogruMu = false;
+
+            }
+
+            if (dogruYol)
+            {
+                if (yP == 0)
+                {
+
+                }
+                else
+                {
+                    labirentBoyut[xP, yP - 1].BackColor = Color.Gray;
+                    labirentBoyut[xP, yP].BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void timerOyun_Tick(object sender, EventArgs e)
+        {
+            progressBarZaman.Value = sayac;
+            labelZaman.Text = sayac.ToString();
+            if (sayac == 0)
+            {
+                timerOyun.Stop();
+                MessageBox.Show("Oyun Bitti");
+            }
+            sayac--;
+        }
+
+        public void Control(int S, int A)
+        {
+            if (A == 0) { buttonUst.Visible = false; } else { buttonUst.Visible = true; }
+            if (A == 9) { buttonAlt.Visible = false; } else { buttonAlt.Visible = true; }
+            if (S == 0) { buttonSol.Visible = false; } else { buttonSol.Visible = true; }
+            if (S == 9) { buttonSag.Visible = false; } else { buttonSag.Visible = true; }
+
+            if (S == 1 || A == 1) { timerOyun.Start(); }
+            if (S == 9 && A == 9)
+            {
+                timerOyun.Stop();
+                MessageBox.Show("Oyun Bitti");
+            }
+        }
+
+    }
+}
